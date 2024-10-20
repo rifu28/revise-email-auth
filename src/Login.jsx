39c79b988fb4db3ffc550fb5872f8a76dfb0881b
rifu28 <rifatmeumeu28@gmail.com>
2024-10-20 11:04:49 +0600

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Here</h2>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root.jsx";
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
